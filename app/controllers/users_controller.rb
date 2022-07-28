e9cd29ec73a9fc14c99fc93b9aa59dd665183f16
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-07-28 16:41:15 -0700

    Update: view and controller to pass through recipes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     if current_user
       @user = current_user
       @items = UserFacade.user_items(session[:email])
+      @recipes = RecipeFacade.find_recipes(@items)
     else
       redirect_to '/'
       flash[:error] = "Please login to view your dashboard"
