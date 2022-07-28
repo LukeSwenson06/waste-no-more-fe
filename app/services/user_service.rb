0fde002a02eb31e02c1cc0bc0738c4a641bcb358
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-07-28 11:33:55 -0700

    Update: User services and user show page

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,11 +1,7 @@
-class UserService
+class UserService < BaseService
 
   # def self.find_or_create_user(email, name)
   #   response = connection.get('/api/v1/user')
-  # end
-
-  # def self.connection
-  #   Faraday.new(url: 'http://localhost:3000')
   # end
 
 end
