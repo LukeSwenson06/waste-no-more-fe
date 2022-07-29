75e08637fe0439e806a1a74e2609fbbf1ed27dc3
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-07-29 14:27:48 -0700

    Add/Update: services for base, item, recipe and user
    Co-authored-by: luke <lukeswenson95@outlook.com>

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,7 +1,23 @@
-class RecipeService
+class RecipeService < BaseService
     def self.find_recipes_call(ingredients)
         # ingrediets listed as ruby objects at the moment
         # response = connection.get("")
+    #     ingredients = food.map do |f|
+    #   f.name
+    # end.join(',')
+        response = spoonacular_connection.get("/recipes/findByIngredients?ingredients=#{ingredients}") do |faraday|
+            faraday.params['number'] = 10
+            faraday.params['apiKey'] = ENV['recipe_api_key']
+        end
+        JSON.parse(response.body, symbolize_names: true)
     end
+
+    def self.recipe_details_call(id)
+        response = spoonacular_connection.get("/recipes/#{id}/information") do |faraday|
+            faraday.params['apiKey'] = ENV['recipe_api_key']
+        end
+        JSON.parse(response.body, symbolize_names: true)
+    end
+    
     
 end
