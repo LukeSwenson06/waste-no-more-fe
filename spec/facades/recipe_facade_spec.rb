f204f300b437a032eb3ee2e9b52b2b2f7278c452
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-07-29 14:18:32 -0700

    Test: add recipe facade test
    Co-authored-by: luke <lukeswenson95@outlook.com>

---
+++ spec/facades/recipe_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFacade do
+  describe 'methods' do
+    it 'returns a list of recipes' do
+      ingredients = "milk,cookies"
+      recipes = RecipeFacade.find_recipes(ingredients)
+
+      expect(recipes).to be_all(Recipe)
+    end
+
+    it 'returns details fo a recipe' do
+      recipe_detials = RecipeFacade.recipe_details(991625)
+      
+      expect(recipe_detials).to be_a(RecipeDetails)
+    end
+  end
+end
