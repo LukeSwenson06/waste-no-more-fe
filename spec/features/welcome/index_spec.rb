d0f1895a470c20bc0b9777e3259aeb883a3a531a
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-30 22:22:54 -0600

    Test: add omniauth mock to welcome index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 RSpec.describe "Welcome page" do
 
   it 'has a link to login which uses OAuth and redirects to user show page once logged in' do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      :info => {
+        :name => "Tom",
+        :email => "tom@gmail.com"
+      },
+      :credentials => {
+        :token => "123048139841983"
+      }
+    })
+    user = User.new({name: "Tom", email: "Tom@gmail.com"})
+    allow(UserFacade).to receive(:create_user).and_return(user)
     visit '/'
 
     expect(page).to have_button('Login With Google')
     expect(page).to have_content('Waste No More')
     
+    click_button("Login With Google")
+    expect(current_path).to eq('/dashboard')
+    OmniAuth.config.test_mode = false
   end
 
 end
