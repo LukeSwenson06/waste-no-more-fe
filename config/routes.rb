2222ad253f3a49619f66d9eb945ef0b4ae992592
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-31 11:48:50 -0600

    Config: added routes for foodbanks show, index, and search

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#show'
 
   resources :items, only: [:new, :create]
+
+  get '/foodbanks', to:'foodbanks#search'
+  get '/foodbanks/index', to: 'foodbanks#index'
+  get '/foodbanks/:id', to: 'foodbanks#show'
+
+  get '/foodbanks/search', to:'foodbanks#search'
 end
