990740348c9fd2e6bbc30fbaaa1383e0caf9405a
tjhaines-cap <tjhaines@eckerd.edu>
2022-07-30 22:24:55 -0600

    Feat: update post request in user service for creating user upon login

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -4,12 +4,14 @@ class UserService < BaseService
   #   response = local_connection.get('/api/v1/user')
   # end
   def self.create_user_call(auth_hash)
-    response = local_connection.post('/api/v1/users'), {
-      name: auth_hash['info']['email'],
-      email: auth_hash['info']['name'],
-      image: auth_hash['info']['image'],
-      token: auth_hash['credentials']['token']
-    }.to_json
+    response = connection.post('/api/v1/users', "Content-Type" => "application/json") do |req|
+      req.body = {
+       name: auth_hash['info']['name'],
+       email: auth_hash['info']['email'],
+       image: auth_hash['info']['image'],
+       token: auth_hash['credentials']['token']
+     }
+   end
   end
 
 
