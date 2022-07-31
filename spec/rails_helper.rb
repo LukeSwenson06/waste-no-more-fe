5f59bc71546589dd103d40da2ff1590c67fc0979
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-31 11:49:20 -0600

    Rails: added VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,11 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes' 
+  config.hook_into :webmock
+  config.filter_sensitive_data('<api_key>') { ENV['api_key'] }
+  config.configure_rspec_metadata!
+end
