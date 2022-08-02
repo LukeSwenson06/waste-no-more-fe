5f1d10389b51b399913d3f76c8566dcfb8d63a0d
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-02 10:58:41 -0700

    Refactored: specs

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -32,5 +32,59 @@ RSpec.describe "Welcome page" do
     expect(page).to have_content("Hello, #{@user.name}! Welcome To Your Fridge")
     OmniAuth.config.test_mode = false
   end
+  
+  describe "navigation bar" do
+    it "has a link to log out" do
+      OmniAuth.config.silence_get_warning = true
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+        :info => {
+          :name => "Tom",
+          :email => "tom@gmail.com"
+        },
+        :credentials => {
+          :token => "123048139841983"
+        }
+        })
+        user = User.new({name: "Tom", email: "Tom@gmail.com"})
+        allow(UserFacade).to receive(:create_user).and_return(user)
+        @user = User.new({name: "Tom", email: "Tom@gmail.com"})
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        allow(UserFacade).to receive(:user_items).and_return([])
+
+
+        visit '/'
+
+        expect(page).to have_button('Login With Google')
+        expect(page).to have_content('Waste No More')
+        
+        click_button("Login With Google")
+        expect(current_path).to eq('/dashboard')
+
+        expect(page).to have_content("Hello, #{@user.name}! Welcome To Your Fridge")
+        OmniAuth.config.test_mode = false
+      within ".links" do
+        click_link 'Log Out'
+        expect(current_path).to eq('/') 
+      end
+    end
+
+    it "has a link to resources", :vcr do
+      visit '/'
+      within ".links" do
+        click_link 'Resources'
+        expect(current_path).to eq('/resources') 
+      end
+    end
+
+    it "has a link to foodback index" do
+      visit '/'
+      within ".links" do
+        click_link 'Foodbanks'
+        expect(current_path).to eq('/foodbanks') 
+      end
+    end
+  end
+  
 
 end
