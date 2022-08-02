2131242ca08de1e988dde3d410a84b7ce57e2269
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-02 16:01:04 -0600

    Feat: categorize items in user facade user items call

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -11,14 +11,24 @@ class UserFacade
 
   def self.user_items(email)
     json = UserService.get_user_items(email)
-    if json[:data]
-      items = json[:data].map do |item_data|
-        Item.new(item_data[:attributes])
-      end
-    else
-      items = nil
+    items = json[:data].map do |item_data|
+      Item.new(item_data[:attributes])
     end
-    return items
+    if items != []
+      items_categorized = {expired: [], expiring: [], fresh: []}
+      items.each do |item|
+        if item.days_to_expired(Date.today) <= 0
+          items_categorized[:expired] << item
+        elsif 0 < item.days_to_expired(Date.today) && item.days_to_expired(Date.today) <= 2
+          items_categorized[:expiring] << item
+        else
+          items_categorized[:fresh] << item
+        end
+      end
+      return items_categorized
+    else
+      return items
+    end
   end
 
 end
