b28845bba86484350fae9c9d7bb986b30b936f0c
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-02 08:37:03 -0600

    updated fixture file summary and readjusted test

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -14,16 +14,9 @@ RSpec.describe 'Recipe Show Page' do
 
     it "displays all the recipes attributes", :vcr do
 
-      summary = "Buffalo Chicken Wings Wonton Wraps might be just the hor d'oeuvre you are searching for. 
-      One portion of this dish contains roughly 2g of protein, 1g of fat, and a total of 39 calories. This recipe serves 20 and costs 13 cents per serving.
-      5 people have tried and liked this recipe. It is perfect for The Super Bowl. Head to the store and pick up wonton wrappers, hot sauce, weight cheddar cheese, and a few other things to make it today.
-      Not a lot of people really liked this Chinese dish. It is brought to you by Foodista. From preparation to the plate, this recipe takes around around 45 minutes. All things considered, we decided this recipe deserves a spoonacular score of 7%.
-      This score is very bad (but still fixable). Similar recipes include Skinny Buffalo Chicken Wonton Cups, Veggie Pizzan and Buffalo Chicken Salad Wonton Cup Appetizers, and Buffalo Chicken Wings."
-
       visit "/recipes/#{@recipe[:id]}"
       expect(page).to have_content(@recipe[:title])
-      # expect(page).to have_content(strip_tags(@recipe[:summary]))
-      expect(page).to have_content(summary)
+      expect(page).to have_content(@recipe[:summary])
       expect(page).to have_content(@recipe[:vegetarian])
       expect(page).to have_content(@recipe[:vegan])
       expect(page).to have_content(@recipe[:glutenFree])
